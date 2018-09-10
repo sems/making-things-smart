20ca3ef2ed87ed0d8907df58081a3eb266b5c28f
Sem <semspanhaak@gmail.com>
2018-09-10 09:43:17 +0200

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from sense_hat import SenseHat
+from time from sleep
+
+sense = SenseHat()
+sense.clear()
+
+white = (255,255,255)
+blue = (0, 0, 255)
+
+#Function
+
+
+#Running loop
+
+while True:
+	sleep(0.25)
+
