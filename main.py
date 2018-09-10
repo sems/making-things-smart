02abed0a4f54f08b8681a11dcfa6790f4a7d69ec
Joris Vos <jorisvos037@gmail.com>
2018-09-10 10:09:19 +0200

    Added colors and color preset

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ sense.clear()
 
 white = (255,255,255)
 blue = (0, 0, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+orange = (255, 165, 0)
+yellow = (255, 255, 0)
+cyan = (0, 255, 255)
+magenta = (255, 0, 255)
+brown = (165, 42, 42)
+grey = (190, 190, 190)
+
+color_presets = [white, red, green, blue, orange, yellow, cyan, magenta, brown, grey]
 
 # Functions ----------------
 def joystick_move_up(event):
