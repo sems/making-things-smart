c9d28107dfe16d0e6b4c71921ecedd3b3a0898cd
Joris Vos <jorisvos037@gmail.com>
2018-09-10 09:51:04 +0200

    Added joystick event listener

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import signal
 from sense_hat import SenseHat
 from time from sleep
 
@@ -8,17 +9,30 @@ white = (255,255,255)
 blue = (0, 0, 255)
 
 # Functions ----------------
-def joystick_move_up:
+def joystick_move_up(event):
 	sense.clear()
-def joystick_move_down:
+def joystick_move_down(event):
 	sense.clear()
-def joystick_move_left:
+def joystick_move_left(event):
 	sense.clear()
-def joystick_move_right:
+def joystick_move_right(event):
 	sense.clear()
-def joystick_move_click:
+def joystick_move_click(event):
 	sense.clear()
 
+def clear():
+	sense.clear()
+
+def exit(signal, frame):
+	clear()
+	print("Bye!")
+	sys.exit(0)
+
 # Main program -------------
-while True:
-	sleep(0.25)
+if __name__ == '__init__':
+	sense.stick.direction_up = joystick_move_up
+	sense.stick.direction_down = joystick_move_down
+	sense.stick.direction_left = joystick_move_left
+	sense.stick.direction_right = joystick_move_right
+
+	signal.signal(signal.SIGING, exit)
